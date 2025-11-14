228515af3b6179abc9e79010879dcb2e4aea1976
Rafael Avelar Batista De Jesus <avela019@ahl02.agate.msi.umn.edu>
2025-11-14 09:29:50 -0600

    big fixes

---
+++ src/testing_functions.py
@@ -0,0 +1,91 @@
+from tqdm import tqdm
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+from src.criterion import *
+
+def test_model(model, test_loader):
+    
+    model.eval()
+    test_bar = tqdm(test_loader, desc=f"[Validation]")
+    metrics = np.zeros((3,))
+    
+    with torch.no_grad():
+        for img_list, ds_img_list, mask in test_bar:
+
+            central_slice = img_list[0][:,1,:,:].unsqueeze(1)
+
+            seg_out, vae_out, mu, logvar = model(ds_img_list[0])
+
+            metrics[0] += soft_dice_coeff(seg_out, mask).mean()
+            metrics[1] += MSE_loss(vae_out, central_slice)
+            metrics[2] += kl_loss(mu, logvar)
+        
+    metrics = metrics / len(test_bar)
+    
+    print(f"--- Validation results --- DICE: {metrics[0]:3f}, MSE: {metrics[1]:3f}, KL {metrics[2]:3f}")
+    
+    return metrics
+
+def bin_mask_2_multi(mask):
+    num_classes = mask.shape[0]
+    multi_mask = torch.zeros(mask.shape[1:3])
+    for i in range(num_classes):
+        multi_mask[mask[i,:,:]==1] = i+1
+    return multi_mask
+    
+    
+def plot_examples(model, test_dataset, slices, save_path):
+    
+    model.eval()
+    print("Plotting results" + "-"*60)
+    
+    with torch.no_grad():
+        j = 0
+        for i in slices:
+            
+            img_list, ds_img_list, mask = test_dataset[i]
+            
+            mask = mask.unsqueeze(0)
+            central_idx = img_list[0].shape[0]//2
+
+            central_slice = img_list[0][central_idx,:,:].unsqueeze(0).unsqueeze(0)
+            
+
+            seg_out, vae_out, mu, logvar = model(ds_img_list[0].unsqueeze(0))
+        
+            dice_coeff = soft_dice_coeff(seg_out, mask).mean()
+            
+            mse_loss = MSE_loss(vae_out, central_slice)
+
+            central_ds_slice = ds_img_list[0][central_idx,:,:].squeeze()
+            seg_out = seg_out.long()
+            
+            seg_out_multi = bin_mask_2_multi(seg_out.squeeze())
+            mask_multi = bin_mask_2_multi(mask.squeeze())
+
+
+            fig, ax = plt.subplots(2,2,figsize = (10,5))
+            plt.gray()
+            ax[0,0].imshow(central_slice.cpu().squeeze())
+            ax[0,0].set_title("HR central slice")
+            ax[0,1].imshow(mask_multi.cpu())
+            ax[0,1].set_title("HR true mask")
+            ax[1,0].imshow(seg_out_multi.cpu())
+            ax[1,0].set_title(f"Predicted mask | Dice = {dice_coeff:.3f}")
+            ax[1,1].imshow(vae_out.squeeze().cpu())
+            ax[1,1].set_title(f"VAE output | MSE = {mse_loss:.3f}")
+            
+            for ax in fig.get_axes():
+                ax.set_xticklabels([])
+                ax.set_yticklabels([])
+                ax.tick_params(axis='x', length=0)
+                ax.tick_params(axis='y', length=0)
+            
+            fig.tight_layout()
+            fig.show()
+            fig.savefig(save_path / f"out_{j}.png")
+            j += 1
+        
+    
+    
